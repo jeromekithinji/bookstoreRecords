9ed2c4a3c7690fb9e7fc6fcf30c3e9d2773adfa6
mosope.dev <mosope.adeyemi.dev@gmail.com>
2024-03-23 08:55:07 -0400

    finshing touches to javadocs

--- Exceptions/TooFewFieldsException.java
+++ Exceptions/TooFewFieldsException.java
@@ -1,3 +1,15 @@
+/**
+ * COMP 6481 (Winter 2024) 
+ * Assignment #2
+ * Due Date: Sunday, March 24th, 2024.
+ * 
+ * © Oluwamosope Adeyemi. ID: 40293741
+ * © Jerome Kithinji. ID: 40280348
+ * 
+ * Written by: 
+ * Jerome Kithinji. ID: 40280348
+ * Oluwamosope Adeyemi. ID: 40293741
+ */
 package Exceptions;
 
 public class TooFewFieldsException extends Exception {
